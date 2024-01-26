71aa8cc56e31e9cf2057ac1aad1a2c41c87aee87
Cautious <somdeliegraphicdesign@gmail.com>
2024-01-26 09:43:32 -0800

    Updated

--- components/client/home/About.jsx
+++ components/client/home/About.jsx
@@ -1,8 +1,9 @@
 "use client";
-import { Avatar } from "@mui/material";
+import { Avatar, Rating } from "@mui/material";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
 const listItems = [
   "World's largest freelance marketplace",
@@ -29,6 +30,62 @@ const data = [
   },
   // Add more objects as needed
 ];
+const data2 = [
+  {
+    title: "The best talent",
+    disc: "Discover reliable professionals by exploring their portfolios and immersing yourself in the feedback shared on their profiles.",
+  },
+  {
+    title: "Fast bids",
+    disc: "Get quick, no-obligation quotes from skilled freelancers. 80% of jobs receive bids within 60 seconds. Your idea is just moments from reality.",
+  },
+  {
+    title: "Quality work",
+    disc: "With Freelancer's talent pool of over 60 million professionals at your fingertips, you'll find quality talent to get what you need done.",
+  },
+  {
+    title: "Be in control",
+    disc: "Stay in the loop while on the move. Chat with your freelancers and get real time updates with our mobile app. Anytime, anywhere.",
+  },
+  // Add more objects as needed
+];
+
+const freelancers = [
+  {
+    firstName: "John",
+    surname: "Doe",
+    profession: "Web Developer",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    rating: 4.8,
+  },
+  {
+    firstName: "Jane",
+    surname: "Smith",
+    profession: "Graphic Designer",
+    skills: ["Adobe Photoshop", "Adobe Illustrator", "Sketch"],
+    rating: 4.5,
+  },
+  {
+    firstName: "Michael",
+    surname: "Johnson",
+    profession: "Copywriter",
+    skills: ["Copywriting", "Content Marketing", "SEO"],
+    rating: 4.7,
+  },
+  {
+    firstName: "Sarah",
+    surname: "Lee",
+    profession: "UI/UX Designer",
+    skills: [
+      "User Interface Design",
+      "User Experience Design",
+      "Figma",
+      "Sketch",
+    ],
+    rating: 4.9,
+  },
+  // Add more freelancers as needed
+];
 
 const About = () => {
   const variants = {
@@ -40,57 +97,120 @@ const About = () => {
   };
 
   return (
-    <div className="grid gap-4 pt-6 sm:grid-cols-2">
-      <div className="p-4 sm:p-0 ">
-        {/* <h1 className="sm:text-[44px] text-2xl font-semibold">
+    <div className="">
+      <div className="grid gap-4 pt-6 sm:grid-cols-2">
+        <div className="p-4 sm:p-0 ">
+          {/* <h1 className="sm:text-[44px] text-2xl font-semibold">
           Make it real with Freelancer
         </h1> */}
-        <h2 className="sm:text-4xl text-2xl font-semibold">
-          Make it real with Freelancer
-        </h2>
-        <motion.ul
-          initial="hidden"
-          animate="visible"
-          variants={variants}
-          className=" mt-4 "
-        >
-          {listItems?.map((item, index) => (
-            <motion.li
-              variants={variants}
-              className="flex items-center gap-2 mt-2"
-              key={index}
-            >
-              <span className="w-4 h-4 rounded-full bg-roseRed"></span>
-              {item}
-            </motion.li>
-          ))}
-        </motion.ul>
+          <h2 className="sm:text-4xl text-2xl font-semibold">
+            <span className="text-roseRed">Make it real</span> with Freelancer
+          </h2>
+          <motion.ul
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            className=" mt-4 "
+          >
+            {listItems?.map((item, index) => (
+              <motion.li
+                variants={variants}
+                className="flex items-center gap-2 mt-2"
+                key={index}
+              >
+                <span className="w-4 h-4 rounded-full bg-roseRed"></span>
+                {item}
+              </motion.li>
+            ))}
+          </motion.ul>
+        </div>
+        <div className="relative anim mx-auto border-gray-300 dark:border-gray-800 mt-4 sm:mt-0 -z-10 bg-gray-300 dark:bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] sm:h-[300px] w-[300px] sm:w-[600px]">
+          <div className="h-[32px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
+          <div className="h-[46px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+          <div className="h-[46px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+          <div className="h-[64px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+          <div className="rounded-[2rem] overflow-hidden h-[572px] w-[272px] sm:h-[272px] sm:w-[572px] bg-white dark:bg-gray-800">
+            <div className="dark:hidden h-[272px] w-[100%] sm:w-[572px] p-4 ">
+              <h1 className="text-center mt-3 text-lg font-semibold text-roseRed">
+                How It Works
+              </h1>
+              <div className="grid md:grid-cols-3 pt-6 sm:pt-0 mt-4 sm:mt-2 items-start justify-center gap-3 w-full sm:h-[80%]">
+                {data?.map((item, index) => (
+                  <div key={index} className="border-2 p-2 h-full w-[100%]">
+                    <Image
+                      src={item?.icon}
+                      alt="Precedent logo"
+                      width={40}
+                      height={40}
+                      className=""
+                    />
+                    <span>{item?.title}</span>
+                    <p className="text-xs">{item?.disc}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="relative mx-auto border-gray-300 dark:border-gray-800 mt-4 sm:mt-0 -z-10 bg-gray-300 dark:bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] sm:h-[300px] w-[300px] sm:w-[600px]">
-        <div className="h-[32px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
-        <div className="h-[46px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-        <div className="h-[46px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-        <div className="h-[64px] w-[3px] bg-gray-300 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-        <div className="rounded-[2rem] overflow-hidden h-[572px] w-[272px] sm:h-[272px] sm:w-[572px] bg-white dark:bg-gray-800">
-          <div className="dark:hidden h-[272px] w-[100%] sm:w-[572px] p-4 ">
-            <h1 className="text-center mt-3 text-lg font-semibold text-roseRed">
-              How It Works
-            </h1>
-            <div className="grid md:grid-cols-3 pt-6 sm:pt-0 mt-4 sm:mt-2 items-start justify-center gap-3 w-full sm:h-[80%]">
-              {data?.map((item, index) => (
-                <div key={index} className="border-2 p-2 h-full w-[100%]">
-                  <Image
-                    src={item?.icon}
-                    alt="Precedent logo"
-                    width={40}
-                    height={40}
-                    className=""
-                  />
-                  <span>{item?.title}</span>
-                  <p className="text-xs">{item?.disc}</p>
-                </div>
+      <div className="p-4 sm:p-0 ">
+        <div className="grid sm:grid-cols-2 gap-6  mt-4">
+          <div className="">
+            <div className="flex justify-between">
+              <h3 className="">Top Rated Freelancers</h3>{" "}
+              <Link href="#" className="text-green flex items-center gap-1">
+                see more
+                <FaArrowRight />
+              </Link>
+            </div>
+            <div className="grid sm:grid-cols-2 mt-3 gap-3">
+              {freelancers?.map((user, index) => (
+                <motion.div
+                  className="w-full flex flex-col bg-gray-300/50 p-3 shadow"
+                  key={index}
+                >
+                  <div className="flex items-center border-b border-gray-400 py-2 justify-between">
+                    <div className="flex items-center gap-2">
+                      <Avatar>{user?.firstName.charAt(0)}</Avatar>
+                      <h3 className="text-lg flex items-center gap-3 text-gray-700 font-semibold">
+                        {user?.firstName} {user?.surname.charAt(0)}
+                      </h3>
+                    </div>
+                    <p className="text-sm">{user?.profession}</p>
+                  </div>
+                  <div className="flex flex-col flex-grow mt-2 text-sm ">
+                    {user?.skills.slice(0, 3).map((skill, index) => (
+                      <span
+                        key={index}
+                        className="flex gap-2 text-gray-400 items-center"
+                      >
+                        <span className="w-2 h-2 rounded-full bg-green"></span>
+                        {skill}
+                      </span>
+                    ))}
+                  </div>
+                  <p className="flex items-center flex-grow mt-2 text-sm justify-between">
+                    <Rating readOnly value={user?.rating} className="mui" />
+                    <Link
+                      href="#"
+                      className="bg-green hover:bg-transparent transition hover:text-green hover:border-green border text-gray-100 px-2 py-1 rounded"
+                    >
+                      Hire me!
+                    </Link>
+                  </p>
+                </motion.div>
               ))}
             </div>
+          </div>
+          <div className="grid sm:grid-cols-2 gap-4">
+            {data2?.map((item, index) => (
+              <motion.div className="w-full" key={index}>
+                <h3 className="text-lg text-roseRed font-semibold">
+                  {item.title}
+                </h3>
+                <p className="text-sm">{item.disc}</p>
+              </motion.div>
+            ))}
           </div>
         </div>
       </div>
--- components/client/home/Hero.jsx
+++ components/client/home/Hero.jsx
@@ -36,35 +36,59 @@ const Hero = () => {
     },
     hidden: { opacity: 0 },
   };
+  const textVariants = {
+    initial: {
+      x: -500,
+      opacity: 0,
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.1,
+      },
+    },
+  };
 
   return (
     <div className="flex px-4 sm:px-1 flex-col items-center justify-center min-h-screen gap-6">
-      <div className="w-90% sm:w-[70%] mx-auto">
-        <h1 className="text-2xl sm:text-3xl md:text-6xl font-bold tracking-[-0.02em] ">
+      <motion.div
+        variants={textVariants}
+        initial="initial"
+        animate="animate"
+        className="w-90% sm:w-[70%] mx-auto "
+      >
+        <motion.h1
+          variants={textVariants}
+          className="text-2xl sm:text-3xl md:text-6xl font-bold tracking-[-0.02em] "
+        >
           <span className="text-roseRed">Hire the best freelancers</span> for
           any job, online.
-        </h1>
-        <p className="mt-6 text-gray-600 md:text-xl">
+        </motion.h1>
+        <motion.p
+          variants={textVariants}
+          className="mt-6 text-gray-600 md:text-xl"
+        >
           Every day we’re helping thousands of companies to hire the best talent
           from our profiled community of 110 + MLN job seekers and via our large
           network of quality affiliate partners.
-        </p>
-      </div>
-
-      <div className="mx-auto mt-6 text-xs sm:text-base flex items-center justify-center gap-2 sm:gap-4">
-        <Link
-          href="#"
-          className="group flex max-w-fit sm:items-center items-start justify-center space-x-2 rounded-full border border-green bg-green px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-gray-700"
-        >
-          <HiRectangleGroup /> Hire a Freelancer
-        </Link>
-        <Link
-          href="#"
-          className="flex max-w-fit sm:items-center items-start justify-center space-x-2 rounded-full border border-green bg-white px-5 py-2 text-sm text-gray-700 shadow-md transition-colors hover:border-roseRed"
-        >
-          <FaMoneyCheckAlt /> Become a Freelancer
-        </Link>
-      </div>
+        </motion.p>
+        <div className="mx-auto mt-6 text-xs sm:w-1/2 sm:text-base grid sm:grid-cols-2 justify-center gap-2 sm:gap-4">
+          <Link
+            href="#"
+            className="group flex whitespace-nowrap sm:items-center items-center justify-center gap-2 rounded-full border border-green bg-green px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-gray-700"
+          >
+            <HiRectangleGroup /> Hire a Freelancer
+          </Link>
+          <Link
+            href="#"
+            className="flex whitespace-nowrap sm:items-center items-center justify-center gap-2 rounded-full border border-green bg-white px-5 py-2 text-sm text-gray-700 shadow-md transition-colors hover:border-roseRed"
+          >
+            <FaMoneyCheckAlt /> Become a Freelancer
+          </Link>
+        </div>
+      </motion.div>
 
       <motion.div
         initial="hidden"
@@ -88,7 +112,6 @@ const Hero = () => {
           </motion.div>
         ))}
       </motion.div>
-
       {/* <p className="mt-6 animate-fade-up text-center text-gray-500 md:text-xl">
         We're different to traditional candidate-search platforms. We deliver
         the most relevant, qualified candidates on demand through smart
